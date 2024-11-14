c6f1db1d471182c54f5320b1eae483493c60cf5e
Abbas Majidov <69363088+absmj@users.noreply.github.com>
2024-11-14 14:50:26 +0400

    Update Dashboard.php

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -1,6 +1,7 @@
 <?php
-defined('BASEPATH') or exit('No direct script access allowed');
-require_once APPPATH . "custom/BaseController.php";
+defined('BASE_PATH')           OR define('BASE_PATH', '/modules/psd_analytic/');
+defined('BASE_URL_REQUEST')           OR define('BASE_URL_REQUEST', '/psd_analytic/');
+require_once FCPATH . BASE_PATH .  "/custom/BaseController.php";
 
 class Dashboard extends BaseController
 {
@@ -9,6 +10,13 @@ class Dashboard extends BaseController
 	public function __construct()
 	{
 		parent::__construct();
+		// autorize();
+		if(ENVIRONMENT != 'development') {
+			$username = strtolower($_SESSION['user_login']);
+			if(!in_array($username, ['khanalid', 'shahriyara', 'alasgara', 'abbasm', 'zahraag'])) {
+				die("Sizin icazəniz yoxdur");
+			}
+		}
 
 	}
 
@@ -25,70 +33,85 @@ class Dashboard extends BaseController
 			$post = json_decode(file_get_contents("php://input"), 1);
 			$this->load->model("Page_model", "page");
 			$this->load->model("Chart_model", "chart");
-
 			$page_id = $this->page->insert([
-				"title" => $post['title'],
+				'title' => $post['title'],
+				// "template" => $this->input->get('template'),
 				"report_id" => $report_id,
-				"access" => $post["access"]
+				"access" => $post['access'] ?? []
 			]);
-			
+
 
 			foreach($post['charts'] as &$value) {
 				$value['page_id'] = $page_id;
+				unset($value['id']);
 			}
-			// dd($value);
+
 			$data = $this->chart->insertBatch($post['charts']);
 			echo BaseResponse::ok("success", $data);
 			exit;
 		}
 
-		$this->set("styles", [
-				"css/folder.css"
-			])
-			->set("vendorStyles", [
-				"vendor/pivottable/pivot.css",
-				"vendor/datatables/datatables.css",
-				"vendor/gridstack/gridstack.css",
-				])			
-			->set("vendorScripts", [
-				"vendor/datatables/datatables.js",
-				"vendor/pivottable/pivottable.js",
-				"vendor/gridstack/gridstack.js"
-			])
-			->set("vendorScripts", [
-				"js/functions.js",
-				"js/mock-data.js"
-			])
-			->set("scripts", [
-				"js/chart-visual.js",
-				"js/template.js",
-				"js/dashboard-create.js",
-			]);
+		// $this->set("styles", [
+		// 		"css/folder.css"
+		// 	])
+		// 	->set("vendorStyles", [
+		// 		"vendor/pivottable/pivot.css",
+		// 		"vendor/datatables/datatables.css",
+		// 		"vendor/gridstack/gridstack.css",
+		// 		])			
+		// 	->set("vendorScripts", [
+		// 		"vendor/datatables/datatables.js",
+		// 		"vendor/pivottable/pivottable.js",
+		// 		"vendor/gridstack/gridstack.js"
+		// 	])
+		// 	->set("vendorScripts", [
+		// 		"js/functions.js",
+		// 		"js/mock-data.js"
+		// 	])
+		// 	->set("scripts", [
+		// 		"js/chart-visual.js",
+		// 		"js/template.js",
+		// 		"js/dashboard-create.js",
+		// 	]);
 
-			$this->set("vendorScripts", [
-				"vendor/codemirror/codemirror.js",
-				"vendor/codemirror/autorefresh.js",
-				"vendor/codemirror/match-brackets.js",
-				"vendor/codemirror/js/javascript.js",
-			])
-				->set("vendorStyles", [
-					"vendor/codemirror/codemirror.css",
-				]);
+		// 	$this->set("vendorScripts", [
+		// 		"vendor/codemirror/codemirror.js",
+		// 		"vendor/codemirror/autorefresh.js",
+		// 		"vendor/codemirror/match-brackets.js",
+		// 		"vendor/codemirror/js/javascript.js",
+		// 	])
+		// 		->set("vendorStyles", [
+		// 			"vendor/codemirror/codemirror.css",
+		// 		]);
 
 		$this->title = "Səhifə yaradılması";
-
+		
 		$report = $this->report->get($report_id);
+		
+		// if($report['report_table']) {
+		// 	$result = $this->report->getReportData($report['report_table'], 100, json_decode($report['params'] ?? '[]', 1) ?? [],array_keys(json_decode($report['fields_map'] ?? '[]', 1) ?? []), $this->input->get());	
+		// }
 
-		if($report['report_table']) {
-			$result = $this->report->getReportData($report['report_table'], 1000, json_decode($report['params'] ?? '[]', 1),array_keys(json_decode($report['fields_map'] ?? '[]', 1)), $this->input->get());
-			
+
+		if($report['file']) {
+			$result = $this->report->getCsvFile($report['file']);
+			$result = csv2json($result);
+			$result = array_reverse(array_slice($result, 0, 250));
 		}
 
 		$data['result'] = $result ?? [];
-		echo BaseResponse::ok("Success", $result);
-		exit;
+		$data['report'] = $report ?? [];
+		echo BaseResponse::ok("Success", $data['result']);
+		// $this->page("create", $data, false);
+	}
 
-		$this->page("create", $data, false);
+
+	public function templates() {
+		echo BaseResponse::ok("Success", ["view" => $this->view("templates/index", [], true)]);
+	}
+
+	public function template($name = "simple") {
+		echo BaseResponse::ok("Success", ["view" => $this->view("templates/" . $name, [], true)]);
 	}
 
 	public function update($page_id) {
@@ -96,14 +119,42 @@ class Dashboard extends BaseController
 			$post = json_decode(file_get_contents("php://input"), 1);
 			$this->load->model("Page_model", "page");
 			$this->load->model("Chart_model", "chart");
-
-			$page_id = $this->page->update($page_id, [
+			$this->page->update($page_id, [
 				"title" => $post['title'],
-				"access" => $post["access"]
+				"access" => $post['access'] ?? []
 			]);
-			
-			$data = $this->chart->updateBatch("id", $post['charts']);
-			echo BaseResponse::ok("success", $data);
+			$updated = [];
+			$inserted = [];
+			$deleted = [];
+			$currentCharts = $this->chart->findByPageId($page_id);
+			$deleted = [];
+			$newCharts = array_filter($post['charts'] ?? [], function($chart) {
+				return $chart['id'];
+			});
+
+			if(count($newCharts) < count($currentCharts)) {
+				$deleted = array_diff(array_column($currentCharts, "id"), array_column($newCharts, "id"));
+				foreach($deleted as $id) $this->chart->delete($id, "id");
+			}
+
+			foreach($post['charts'] as &$value) {
+				$value['page_id'] = $page_id;
+				if($value['id']) {
+					$updated[] = $value;
+				} else {
+					unset($value['id']);
+					$inserted[] = $value;
+				}
+			}
+
+			if(count($updated) > 0)
+				$this->chart->updateBatch("id", $updated);
+			if(count($inserted) > 0)
+				$this->chart->insertBatch($inserted);
+
+
+
+			echo BaseResponse::ok("success", ["updated" => count($updated), "inserted" => count($inserted), "deleted" => count($deleted)]);
 			exit;
 		}
 	}
