76b332832a15d47353287f1d27be7743bccafcd6
Abbas Majidov <69363088+absmj@users.noreply.github.com>
2024-11-14 14:40:54 +0400

    Update Page_model.php

--- application/models/Page_model.php
+++ application/models/Page_model.php
@@ -7,7 +7,8 @@ class Page_model extends CRUD {
 
     public function list() {
         return $this->db->select("p.*, r.name report_name")->from($this->table . " p")
-                            ->join("reports r", "r.id = report_id")
+                            ->join("psd_analytic_reports r", "r.id = report_id")
+                            ->order_by("p.id", "desc")
                             ->get()
                             ->result_array();
     }
@@ -15,11 +16,11 @@ class Page_model extends CRUD {
     public function get($id) {
         return $this->db->query("
             SELECT 
-                p.id,p.access, p.title page_title, p.template, j.date,r.name,r.id report_id,r.report_table, q.sql,q.params,q.db,q.unique_field,q.fields_map,
+                p.id,p.access,p.title page_title, p.access, p.template, j.date,r.name,r.id report_id,r.report_table, q.sql,q.params,q.db,q.unique_field,q.fields_map,
                 j.id job_id,
                 f.id file_id, f.name file, location, f.created_at file_created_at, f.folder_id
             FROM {$this->table} p
-            JOIN LATERAL (select * from jobs where report_id=p.report_id order by id desc limit 1) j ON true
+            JOIN (select report_id, max(date) date, max(file_id) file_id, max(query_id) query_id, max(id) id from jobs group by report_id order by report_id desc) j ON j.report_id=p.report_id
             JOIN queries q ON q.id = j.query_id
             JOIN reports r ON r.id = j.report_id
             LEFT JOIN files f ON f.id = j.file_id
