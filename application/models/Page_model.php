b1ab00f64e36164773775416d35e35fe7e6114c2
Abbas Majidov <majidovabbas@gmail.com>
2025-04-03 22:45:03 +0400

    dock

--- application/models/Page_model.php
+++ application/models/Page_model.php
@@ -22,13 +22,11 @@ class Page_model extends CRUD
             SELECT 
                 p.id,p.access,p.title page_title, p.access, p.template, j.date,r.name,r.id report_id,r.report_table, q.sql,q.params,q.db,q.unique_field,q.fields_map,
                 j.id job_id,
-                j.date last_file,
-                f.id file_id, f.name file, location, f.created_at file_created_at, f.folder_id
+                j.date last_file
             FROM {$this->table} p
             JOIN (select report_id, max(date) date, max(file_id) file_id, max(query_id) query_id, max(id) id from jobs group by report_id order by report_id desc) j ON j.report_id=p.report_id
             JOIN queries q ON q.id = j.query_id
             JOIN reports r ON r.id = j.report_id
-            LEFT JOIN files f ON f.id = j.file_id
             WHERE p.id = ?
         ", [$id])->row_array();
     }
